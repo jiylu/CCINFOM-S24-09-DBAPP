71df22fa1b1de9f8b39d191cf153890f40b6abc6
jeremyy1231 <jeremy_leano@dlsu.edu.ph>
2025-10-07 18:33:09 +0800

    added skeleton db schema

---
+++ dbapp.sql
@@ -0,0 +1,25 @@
+CREATE SCHEMA IF NOT EXISTS ticketing_system;
+
+USE ticketing_system;
+
+-- Table creation 
+
+CREATE TABLE IF NOT EXISTS employees (
+	-- fields 
+);
+
+CREATE TABLE IF NOT EXISTS technicians (
+	-- fields 
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+	-- fields
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+	-- fields
+);
+
+
+-- Inserts (dummy data) dito
+--
