e6056889493ed9a94bc5a55a080b39de66564e77
jeremyy1231 <jeremy_leano@dlsu.edu.ph>
2025-10-25 22:33:46 +0800

    added departments table in schema and added values to it

--- dbapp.sql
+++ dbapp.sql
@@ -4,21 +4,28 @@ USE ticketing_system;
 
 -- Table creation 
 
-CREATE TABLE IF NOT EXISTS employees (
-	-- fields 
+-- CREATE TABLE IF NOT EXISTS Employees ();
+
+-- CREATE TABLE IF NOT EXISTS Technicians ();
+
+CREATE TABLE IF NOT EXISTS Departments (
+	department_id INT AUTO_INCREMENT PRIMARY KEY,
+	department_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS technicians (
-	-- fields 
-);
+INSERT INTO Departments (department_name)
+VALUES
+	('Human Resources'),
+	('Finance'),
+	('Customer Support'),
+	('Sales'),
+	('Operations'),
+	('Marketing'),
+	('Administration'),
+	('Information Technology');
 
-CREATE TABLE IF NOT EXISTS departments (
-	-- fields
-);
 
-CREATE TABLE IF NOT EXISTS categories (
-	-- fields
-);
+-- CREATE TABLE IF NOT EXISTS Categories ();
 
 
 -- Inserts (dummy data) dito
