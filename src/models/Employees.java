2a4b908d31b87812529ecbf4b8e91b74c2388ddf
m-fides <maria_fides_bancoro@dlsu.edu.ph>
2025-10-27 23:34:50 +0100

    added is_active field and update cosntructor

--- src/models/Employees.java
+++ src/models/Employees.java
@@ -2,22 +2,30 @@ package models;
 
 public class Employees {
     private int emp_id;
+    private int user_id;
     private String last_name;
     private String first_name;
     private int dept_id;
     private String role;
+    private boolean is_active;
 
-    public Employees(int emp_id, String last_name, String first_name, int dept_id, String role) {
+    public Employees(int emp_id, int user_id, String last_name, String first_name, int dept_id, String role, boolean is_active) {
         this.emp_id = emp_id;
+        this.user_id = user_id;
         this.last_name = last_name;
         this.first_name = first_name;
         this.dept_id = dept_id;
         this.role = role;
+        this.is_active = is_active;
     }
 
 // getters
     public int getEmpID() {
         return emp_id;
+    }
+
+    public int getUserID() {
+        return user_id;
     }
 
     public String getLastName() {
@@ -36,7 +44,15 @@ public class Employees {
         return role;
     }
 
+    public boolean isActive() {
+        return is_active;
+    }
+
 // setters for update
+
+    public void setUserID(int user_id) {
+        this.user_id = user_id;
+    }
 
     public void setLastName(String last_name) {
         this.last_name = last_name;
@@ -52,6 +68,10 @@ public class Employees {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public void setIsActive(boolean is_active) {
+        this.is_active = is_active;
     }
 
 }
