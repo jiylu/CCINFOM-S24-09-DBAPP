2914af1ce38313a50515774924837e523b6923ed
Gaibril <gaibrilgregorio@gmail.com>
2025-10-27 19:36:24 +0800

    update on technicians

--- src/models/Technicians.java
+++ src/models/Technicians.java
@@ -4,16 +4,14 @@ public class Technicians {
     private int technician_id;
     private String tech_lastName;
     private String tech_firstName;
-    private String tech_username;
-    private boolean activeTechnician;
+    private int user_id;
     private boolean hasActiveTicket;
 
-    public Technicians(int technician_id, String tech_lastName, String tech_firstName, String tech_username, boolean activeTechnician, boolean hasActiveTicket){
+    public Technicians(int technician_id, String tech_lastName, String tech_firstName, int user_id, boolean hasActiveTicket){
         this.technician_id = technician_id;
         this.tech_lastName = tech_lastName;
         this.tech_firstName = tech_firstName;
-        this.tech_username = tech_username;
-        this.activeTechnician = activeTechnician;
+        this.user_id = user_id;
         this.hasActiveTicket = hasActiveTicket;
     }
 
@@ -29,20 +27,12 @@ public class Technicians {
         return tech_firstName;
     }
 
-    public String getTech_username(){
-        return tech_username;
-    }
-
-    public boolean getActiveTechnician(){
-        return activeTechnician;
+    public int getUser_ID(){
+        return user_id;
     }
 
     public boolean isHasActiveTicket(){
         return isHasActiveTicket();
-    }
-
-    public void setActiveTechnician(boolean activeTechnician){
-        this.activeTechnician = activeTechnician;
     }
 
     public void setHasActiveTicket(boolean hasActiveTicket){
